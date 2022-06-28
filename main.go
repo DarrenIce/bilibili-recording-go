647b2b0abf1e9895c68753e02949d0f5b00bc580
Darren <bhl865931168@gmail.com>
2022-06-28 10:19:58 +0800

    fix: fix fetch bilibili real url.

--- main.go
+++ main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
-	_ "net/http/pprof"
+
 	"os"
 	"runtime"
 	"strings"
@@ -72,12 +70,5 @@ func Init() {
 
 func main() {
 	Init()
-	go func() {
-		// 启动一个 http server，注意 pprof 相关的 handler 已经自动注册过了
-		if err := http.ListenAndServe(":6060", nil); err != nil {
-			log.Fatal(err)
-		}
-		os.Exit(0)
-	}()
-	routers.GIN.Run(":8888")
+	routers.GIN.Run()
 }
