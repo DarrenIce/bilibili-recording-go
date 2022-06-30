2938f3d4e77a7068eab1d7fa9c92903dd89a7403
Darren <bhl865931168@gmail.com>
2022-06-30 13:55:43 +0800

    fix: add live exception exit.

--- live/douyin.go
+++ live/douyin.go
@@ -143,6 +143,8 @@ func (s *douyin) DownloadLive(r *Live) {
 	isLive, dpi, bitRate, fps := GetStreamInfo(s.liveUrl)
 	if !isLive {
 		fmt.Printf("%s[RoomID: %s] 直播状态不正常\n", r.Uname, r.RoomID)
+		r.RecordEndTime = time.Now().Unix()
+		golog.Info(fmt.Sprintf("%s[RoomID: %s] 录制结束", r.Uname, r.RoomID))
 		time.Sleep(120 * time.Second)
 		atomic.CompareAndSwapUint32(&r.State, running, start)
 		return
